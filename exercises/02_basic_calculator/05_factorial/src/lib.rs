ecec2bee227d4775687164dec6cc1716eda8807f
Johannes Sandjaja <johannessandjaja@proton.me>
2024-06-03 18:42:42 +0700

    basic calc finished

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,6 +9,12 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+fn factorial(n: i32) -> i32 {
+    match n {
+        0 | 1 => 1,
+        i => i * factorial(i - 1),
+    }
+}
 
 #[cfg(test)]
 mod tests {
