1594e082b791ff831a77f293444259352c577e4d
Johannes Sandjaja <johannessandjaja@proton.me>
2024-06-06 23:46:05 +0700

    impl add

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,16 +8,74 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
-use std::ops::Add;
-#[derive(Debug)]
-struct SaturatingU16 {
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct SaturatingU16 {
     value: u16,
 }
 
-impl Add for SaturatingU16 {
-    type Output = u16;
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self {
+            value: value.into(),
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        (*value).into()
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        (*value).into()
+    }
+}
+
+// This trait is important so that other impl trait can add u16 to SaturatingU16
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        let sum = self.value.saturating_add(rhs);
+        Self { value: sum }
+    }
+}
+impl std::ops::Add for SaturatingU16 {
+    type Output = SaturatingU16;
 
     fn add(self, rhs: Self) -> Self::Output {
-        self.value + rhs.value
+        self + rhs.value
+    }
+}
+
+// impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+// type Output = SaturatingU16;
+
+// fn add(self, rhs: &SaturatingU16) -> Self::Output {
+// self + *rhs
+// }
+// }
+
+// Implementation for + &u16
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        self + *rhs
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
     }
 }
